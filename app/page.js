82ecf3f0acd9ba48dd61e29017b00b514ba47c04
kajolroy <shrikajol@gmail.com>
2022-12-28 01:16:34 +0600

    setup next with chakra ui:

---
+++ app/page.js
@@ -0,0 +1,11 @@
+"use client";
+import { Box } from "@chakra-ui/react";
+import React from "react";
+import Layout from "./layout";
+export default function page() {
+  return (
+    <Layout>
+      <Box>cdc</Box>
+    </Layout>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,27 @@
-import '../styles/globals.css'
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const theme = extendTheme({
+  semanticTokens: {
+    colors: {
+      error: "red.500",
+      success: "green.500",
+      primary: {
+        default: "red.500",
+        _dark: "red.400",
+      },
+      secondary: {
+        default: "red.800",
+        _dark: "red.700",
+      },
+    },
+  },
+});
+function MyApp({ Component, pageProps }) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
+
+export default MyApp;
